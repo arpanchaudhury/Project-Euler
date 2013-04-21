e5e59e9780b366358d6a10aa4ff4d2e5c5ff6673
Arpan <arpan@Trojan.(none)>
2013-04-21 21:09:42 +0530

    Modification 1: Euler Project Problem 2

--- problem_2/src/main/scala/problem_2.scala
+++ problem_2/src/main/scala/problem_2.scala
@@ -1,8 +1,7 @@
 package euler
 
-object Main {
- 
-  def main(args: Array[String]) = { (fib(4000000, List(2,1)) filter(x => (x%2 == 0))) sum }
+object Main { 
+  def main(args: Array[String]) = println((fib(4000000, List(2,1)) filter(x => (x%2 == 0))) sum)
   
   def fib(ub: Int, f: List[Int]): List[Int] = {
     val n = f.head + f.tail.first
