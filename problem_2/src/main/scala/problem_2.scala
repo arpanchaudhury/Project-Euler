6ec6d3a10b88d9ad1b04b24e53453b55abe01eda
Arpan <arpan@rspc1140.(none)>
2013-04-21 19:38:27 +0530

    Euler Project Problem 2

---
+++ problem_2/src/main/scala/problem_2.scala
@@ -0,0 +1,11 @@
+package euler
+
+object Main {
+ 
+  def main(args: Array[String]) = { (fib(4000000, List(2,1)) filter(x => (x%2 == 0))) sum }
+  
+  def fib(ub: Int, f: List[Int]): List[Int] = {
+    val n = f.head + f.tail.first
+    if(n >= ub) f else fib(ub, n +: f)
+  }
+}
