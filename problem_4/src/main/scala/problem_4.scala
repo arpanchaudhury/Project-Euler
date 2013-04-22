6f7b26fc854d851ffe446eef6e8a27af9b0c03d5
Arpan <arpan@Trojan.(none)>
2013-04-23 01:23:14 +0530

    Temporary commit

--- problem_4/src/main/scala/problem_4.scala
+++ problem_4/src/main/scala/problem_4.scala
@@ -1,5 +1,14 @@
+import scala.util.control.Breaks._
+
 object Main {
-  def main(args: Array[String]) = println(isPalindrome(31213))
+  def main(args: Array[String]) = {
+    for(i <- 999 to 100 by -1) 
+      for (j <- i to 100 by -1) 
+        if (isPalindrome(i * j)) {
+          println((i * j))
+          break
+        }
+  }
   
   def isPalindrome(n: Int): Boolean = {
     val x = countDigits(n, 0)
@@ -8,7 +17,7 @@ object Main {
     else if (n % 10 != (n / math.pow(10, x - 1)).toInt)
       false 
     else 
-      isPalindrome(((n - n % 10 * (math.pow(10, x - 1).toInt + 1)) / 10).toInt)
+      isPalindrome(((n - n % 10 * math.pow(10, x - 1).toInt) / 10).toInt)
   } 
   
   def countDigits(n: Int, count: Int): Int = if (n/10 == 0) (count + 1) else countDigits(n/10, count + 1) 
