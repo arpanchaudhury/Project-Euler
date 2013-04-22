6cad7ba8cff3c94ccc6b18965c7c30e0334bd5a3
Arpan <arpan@Trojan.(none)>
2013-04-22 10:04:49 +0530

    Temporary Commit

--- problem_4/src/main/scala/problem_4.scala
+++ problem_4/src/main/scala/problem_4.scala
@@ -1,9 +1,14 @@
 object Main {
-  def main(args: Array[String]) = println(isPalindrome(555555))
+  def main(args: Array[String]) = println(isPalindrome(31213))
   
   def isPalindrome(n: Int): Boolean = {
-    val x = countDigits(n)
-    if (n % 10 != n / math.pow(10, x - 1)) false else isPalindrome(n - n/10)
+    val x = countDigits(n, 0)
+    if (x == 0 || x == 1)
+      true
+    else if (n % 10 != (n / math.pow(10, x - 1)).toInt)
+      false 
+    else 
+      isPalindrome(((n - n % 10 * (math.pow(10, x - 1).toInt + 1)) / 10).toInt)
   } 
   
   def countDigits(n: Int, count: Int): Int = if (n/10 == 0) (count + 1) else countDigits(n/10, count + 1) 
