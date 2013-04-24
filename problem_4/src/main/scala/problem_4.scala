4eb6716c915e8f1c7bf880ec0e02c7db8ba530bf
Arpan <arpan@rspc1140.(none)>
2013-04-24 20:04:05 +0530

    Modification 1: Euler Project Problem 4

--- problem_4/src/main/scala/problem_4.scala
+++ problem_4/src/main/scala/problem_4.scala
@@ -3,13 +3,11 @@ import scala.util.control.Breaks._
 object Main {
 
   def main(args: Array[String]) = {
-    for (i <- (999*999) to (100*100) by -1) 
-      if (isPalindrome(i) && isValid(i)) {
-        println(i)
-        break
-      }
+    println(test(999 * 999))
   }
   
+  def test(n: Int): Int = if (isPalindrome(n) && isValid(n)) return n else test(n - 1)
+         
   def isPalindrome(n: Int): Boolean = n.toString == n.toString.reverse
 
   def isValid(n: Int): Boolean = {
