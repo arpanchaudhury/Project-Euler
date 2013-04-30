910d59546e23ac3bb3c1a77d9d8f388b60057056
Arpan <arpan@Trojan.(none)>
2013-04-30 07:40:32 +0530

    Euler Project Problem 7

--- problem_7/src/main/scala/problem_7.scala
+++ problem_7/src/main/scala/problem_7.scala
@@ -1,8 +1,12 @@
 object Main {
 
-  def main(args: Array[String]) = println(getNextPrime(getNextPrime(2,List()).head + 1,getNextPrime(2,List())))
+  def main(args: Array[String]) = println(getPrimeList(10001).head)
 
-  def getNextPrime(lb: Int, primes: List[Int]): List[Int] = if (isPrime(lb, primes)) (lb :: primes) else getNextPrime(lb + 1, primes)
+  def getPrimeList(times: Int): List[Int] = if (times == 0) List() else getNextPrime(getPrimeList(times - 1))
+
+  def getNextPrime(primes: List[Int]): List[Int] = if (primes.isEmpty) List(2) else getNextPrimeHelper(primes.head + 1, primes)
+
+  def getNextPrimeHelper(lb: Int, primes: List[Int]): List[Int] = if (isPrime(lb, primes)) (lb :: primes) else getNextPrimeHelper(lb + 1, primes)
 
   def isPrime(n: Int, primes: List[Int]): Boolean = primes match {
     case x :: xs => if (n % x == 0) false else isPrime(n, xs)
