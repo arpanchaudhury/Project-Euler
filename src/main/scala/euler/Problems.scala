484a78a2817edfaaa7a3aed94ed67d8edd7fa36e
Arpan Chaudhury <arpanchaudhury@gmail.com>
2015-10-26 11:12:12 +0530

    added lcm and gcd methods in RichInteger object

--- src/main/scala/euler/Problems.scala
+++ src/main/scala/euler/Problems.scala
@@ -62,7 +62,7 @@ object Problem4 {
 object Problem5 {
 	def exec() {
 		val divisors = (1L to 20L).toStream
-	  	lcm(divisors: _*) match {
+	  	RichInteger.lcm(divisors: _*) match {
 	  		case Some(value) => value.tap("Smallest positive number that is evenly divisible by all of the numbers from 1 to 20")
 	  		case _ => error("Improper range of divisors")
 	  	}
--- src/main/scala/euler/package.scala
+++ src/main/scala/euler/package.scala
@@ -39,31 +39,33 @@ package object euler {
 		def isDivisableBy(divisor: Long): Boolean = longInteger % divisor == 0
 	}
 
-	def gcd(numbers: Long*): Option[Long] = {
-		def gcdOfTwoNumbers(n1: Long, n2: Long): Long = if (n2 == 0L) n1 else gcdOfTwoNumbers(n2, n1 % n2)
+	object RichInteger {
+		def gcd(numbers: Long*): Option[Long] = {
+			def gcdOfTwoNumbers(n1: Long, n2: Long): Long = if (n2 == 0L) n1 else gcdOfTwoNumbers(n2, n1 % n2)
 
-		numbers.length match {
-		case 0 => None
-		case 1 => Some(numbers(0))
-		case _ =>
-			val n1 = numbers(0)
-			val n2 = numbers(1)
-			val rest = numbers.drop(2)
-			val gcdOfFirstTwoNumbers = gcdOfTwoNumbers(n1, n2)
-			gcd((gcdOfFirstTwoNumbers +: rest): _*)
+			numbers.length match {
+			case 0 => None
+			case 1 => Some(numbers(0))
+			case _ =>
+				val n1 = numbers(0)
+				val n2 = numbers(1)
+				val rest = numbers.drop(2)
+				val gcdOfFirstTwoNumbers = gcdOfTwoNumbers(n1, n2)
+				gcd((gcdOfFirstTwoNumbers +: rest): _*)
+			}
 		}
-	}
 
-	def lcm(numbers: Long*): Option[Long] = numbers.length match {
-		case 0 => None
-		case 1 => Some(numbers(0))
-		case _ =>
-			val n1 = numbers(0)
-			val n2 = numbers(1)
-			val rest = numbers.drop(2)
-			val productOfFirstTwoNumbers = n1 * n2
-			val gcdOfFirstTwoNumbers = gcd(n1, n2).get
-			val lcmOfFirstTwoNumbers = productOfFirstTwoNumbers / gcdOfFirstTwoNumbers
-			lcm((lcmOfFirstTwoNumbers +: rest): _*)
+		def lcm(numbers: Long*): Option[Long] = numbers.length match {
+			case 0 => None
+			case 1 => Some(numbers(0))
+			case _ =>
+				val n1 = numbers(0)
+				val n2 = numbers(1)
+				val rest = numbers.drop(2)
+				val productOfFirstTwoNumbers = n1 * n2
+				val gcdOfFirstTwoNumbers = gcd(n1, n2).get
+				val lcmOfFirstTwoNumbers = productOfFirstTwoNumbers / gcdOfFirstTwoNumbers
+				lcm((lcmOfFirstTwoNumbers +: rest): _*)
+		}
 	}
 }
